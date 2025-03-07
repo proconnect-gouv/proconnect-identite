585716c192f6adb6ed0cb038ea81c057b4e903f0
Douglas DUTEIL <douglasduteil@gmail.com>
2025-03-07 14:46:17 +0100

    test(www): move from mocha chai to node:test

--- test/hash-to-postgres-params.test.ts
+++ test/hash-to-postgres-params.test.ts
@@ -1,4 +1,5 @@
-import { assert } from "chai";
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
 import { hashToPostgresParams } from "../src/services/hash-to-postgres-params";
 
 describe("hashToPostgresParams", () => {
