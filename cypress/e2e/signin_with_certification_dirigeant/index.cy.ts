452013d76cb32de592e615c6e297a4e80c71004c
Douglas DUTEIL <douglasduteil@gmail.com>
2025-04-24 00:35:36 +0200

    test(e2e): Elia dirigeante de J. Cook

--- cypress/e2e/signin_with_certification_dirigeant/index.cy.ts
+++ cypress/e2e/signin_with_certification_dirigeant/index.cy.ts
@@ -124,12 +124,12 @@ describe("sign-in with a client requiring certification dirigeant", () => {
 
     cy.title().should("include", "Compte certifié -");
     cy.contains("Vous êtes bien certifié !");
-    cy.contains("Douglas Le Gris");
-    cy.contains("Duteil");
-    cy.contains("fresh+douglasduteil@mail.com");
-    cy.contains("HyyyperProConnectDev4000");
-    cy.contains("Douglas Duteil");
-    cy.contains("Compte certifié");
+    cy.contains("Prénom Douglas Le Gris");
+    cy.contains("Nom Duteil");
+    cy.contains("Email professionnel fresh+douglasduteil@mail.com");
+    cy.contains("Rôle HyyyperProConnectDev4000");
+    cy.contains("Organisation Douglas Duteil");
+    cy.contains("Statut Compte certifié");
     cy.contains("Continuer").click();
 
     cy.title().should("equal", "standard-client - ProConnect");
@@ -195,5 +195,73 @@ describe("connected user should go through the certification flow", function () 
     cy.getDescribed("Clamart").within(() => {
       cy.contains("certifié").should("not.exist");
     });
+  });
+});
+
+describe.only("Signup with a client requiring certification dirigeant", () => {
+  it("shuold welcome Elia Alvernhe as dirigeant of JEREMIE COOK", () => {
+    cy.visit("http://localhost:4000");
+    cy.contains("Forcer une connexion par certification dirigeant").click();
+
+    cy.title().should("include", "S'inscrire ou se connecter - ");
+    cy.contains("Email professionnel").click();
+    cy.focused().type("elia.alvernhe@yopmail.com");
+    cy.contains("Valider").click();
+
+    cy.title().should("include", "Choisir votre mot de passe - ");
+    cy.contains("Mot de passe").click();
+    cy.contains("Recevoir un lien d’identification").click();
+    cy.maildevGetMessageBySubject("Lien de connexion à ProConnect").then(
+      (email) => {
+        cy.maildevVisitMessageById(email.id);
+        cy.contains(
+          "Vous avez demandé un lien d'identification à ProConnect. Utilisez le bouton ci-dessous pour vous connecter instantanément.",
+        );
+        cy.contains("Se connecter").click();
+        cy.maildevDeleteMessageById(email.id);
+      },
+    );
+
+    cy.title().should("include", "Certification dirigeant -");
+    cy.getByLabel("S’identifier avec FranceConnect").click();
+
+    cy.title().should("include", "Connexion 🎭 FranceConnect 🎭");
+    cy.contains("Je suis Elia Alvernhe").click();
+
+    cy.title().should("include", "Déconnexion 🎭 FranceConnect 🎭");
+
+    cy.title().should("include", "Renseigner votre identité -");
+    cy.seeInField("Prénom", "Elia");
+    cy.seeInField("Nom", "Alvernhe");
+    cy.seeInField("Numéro de téléphone professionnel", "");
+    cy.seeInField("Profession ou rôle au sein de votre organisation", "");
+    cy.contains("Profession ou rôle au sein de votre organisation").click();
+    cy.focused().type("Dirigeante");
+    cy.contains("Valider").click();
+
+    cy.title().should("include", "Rejoindre une organisation - ");
+    cy.contains("SIRET de l’organisation que vous représentez").click();
+    cy.focused().clear().type("49430870300052");
+    cy.getByLabel(
+      "Organisation correspondante au SIRET donné : Jeremie Cook",
+    ).click();
+
+    cy.title().should("include", "Compte certifié - ");
+    cy.contains("Vous êtes bien certifié !");
+    cy.contains("Prénom Elia");
+    cy.contains("Nom Alvernhe");
+    cy.contains("Email professionnel elia.alvernhe@yopmail.com");
+    cy.contains("Rôle Dirigeante");
+    cy.contains("Organisation Jeremie Cook");
+    cy.contains("Statut Compte certifié");
+    cy.contains("Continuer").click();
+
+    cy.title().should("equal", "standard-client - ProConnect");
+    cy.contains(
+      '"acr": "https://proconnect.gouv.fr/assurance/certification-dirigeant"',
+    );
+    cy.contains('"email": "elia.alvernhe@yopmail.com",');
+    cy.contains('"siret": "49430870300052",');
+    cy.contains('"label": "Jeremie Cook",');
   });
 });
