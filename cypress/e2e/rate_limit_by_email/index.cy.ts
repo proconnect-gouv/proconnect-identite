28d965cfe548eec9c33d9381338fa9e2e79b6766
Raphaël Dubigny <rdubigny@users.noreply.github.com>
2025-04-25 10:25:27 +0200

    refactor: more human friendly selectors using accessibility layer
    Co-authored-by: Douglas Duteil <douglasduteil@gmail.com>

--- cypress/e2e/rate_limit_by_email/index.cy.ts
+++ cypress/e2e/rate_limit_by_email/index.cy.ts
@@ -5,7 +5,10 @@ describe("trigger rate limit by email", () => {
     cy.login("account-with-totp@yopmail.com");
 
     for (let i = 0; i <= 5; i++) {
-      cy.get("[name=totpToken]").type("123456");
+      cy.contains(
+        "Obtenir un code à usage unique depuis votre application mobile.",
+      ).click();
+      cy.focused().type("123456");
       cy.get(
         '[action="/users/2fa-sign-in-with-authenticator-app"] [type="submit"]',
       ).click();
@@ -20,7 +23,10 @@ describe("trigger rate limit by email", () => {
 
     // trigger reset email verification rate limiter
     for (let i = 0; i <= 10; i++) {
-      cy.get('[name="verify_email_token"]').type("1234567890");
+      cy.contains(
+        "Vérifiez vos emails et insérez le code à 10 chiffres.",
+      ).click();
+      cy.focused().type("1234567890");
       cy.get('[type="submit"]').click();
     }
 
@@ -30,7 +36,8 @@ describe("trigger rate limit by email", () => {
   it("should trigger totp rate limiting", function () {
     // Set email in unauthenticated session
     cy.visit("/users/start-sign-in");
-    cy.get('[name="login"]').type("new-account@yopmail.com");
+    cy.contains("Email professionnel").click();
+    cy.focused().type("new-account@yopmail.com");
     cy.get('[type="submit"]').click();
 
     // trigger reset password rate limiter
