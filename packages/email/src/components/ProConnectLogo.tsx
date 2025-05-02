8a1608302c2b18382270b9e42ce878cd1ed4430c
Rebecca Dumazert <44778533+rebeccadumazert@users.noreply.github.com>
2025-05-02 11:01:14 +0200

    a11y(logo): removes the ‘logo’ specification from art (#1187)

--- packages/email/src/components/ProConnectLogo.tsx
+++ packages/email/src/components/ProConnectLogo.tsx
@@ -4,7 +4,7 @@ export function ProConnectLogo() {
   return (
     <img
       src="https://identite.proconnect.gouv.fr/dist/mail-proconnect.png"
-      alt="ProConnect Logo"
+      alt="ProConnect"
       height={73}
       width={193}
     />
