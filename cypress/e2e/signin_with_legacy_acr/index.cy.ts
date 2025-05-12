5abdd9f471b61aa37aa4a4440b6c65c6861a0147
Douglas DUTEIL <douglas.duteil@beta.gouv.fr>
2025-05-12 21:35:34 +0200

    chore: remove extra users_organizations

--- cypress/e2e/signin_with_legacy_acr/index.cy.ts
+++ cypress/e2e/signin_with_legacy_acr/index.cy.ts
@@ -49,3 +49,16 @@ describe("sign-in with a client requiring eidas1", () => {
     cy.contains('"acr": "eidas1"');
   });
 });
+
+describe("sign-in with a client requiring eidas1", () => {
+  it("should sign-in an return the eidas1 acr value", function () {
+    cy.visit("http://localhost:4000");
+    cy.setRequestedAcrs(["eidas1"]);
+
+    cy.get("button#custom-connection").click({ force: true });
+
+    cy.login("ial1-aal1@yopmail.com");
+
+    cy.contains('"acr": "eidas1"');
+  });
+});
