63a521cf50b32549d2f69359477e024354376edd
Benoit Serrano <benoit.serrano10@gmail.com>
2025-05-27 15:04:42 +0200

    add typing email domain verification type

--- packages/identite/src/types/email-domain.ts
+++ packages/identite/src/types/email-domain.ts
@@ -1,18 +1,29 @@
+export const EMAIL_DOMAIN_APPROVED_VERIFICATION_TYPES = [
+  "official_contact",
+  "trackdechets_postal_mail",
+  "verified",
+] as const;
+
+export const EMAIL_DOMAIN_REJECTED_VERIFICATION_TYPES = [
+  "blacklisted", // unused
+  "external", // domain used by external employees (eg. ext.numerique.gouv.fr)
+  "refused",
+] as const;
+
+export type EmailDomainApprovedVerificationType =
+  (typeof EMAIL_DOMAIN_APPROVED_VERIFICATION_TYPES)[number];
+export type EmailDomainRejectedVerificationType =
+  (typeof EMAIL_DOMAIN_REJECTED_VERIFICATION_TYPES)[number];
+export type EmailDomainVerificationType =
+  | EmailDomainApprovedVerificationType
+  | EmailDomainRejectedVerificationType
+  | null; // domain is not verified, but users are still permitted to use it
+
 export interface EmailDomain {
   id: number;
   organization_id: number;
   domain: string;
-  verification_type: // Unused
-  | "blacklisted"
-    // domain used by external employees (eg. ext.numerique.gouv.fr)
-    | "external"
-    | "refused"
-    // domain is not verified, but users are still permitted to use it
-    | null
-    // the three following verification types can coexist at the same time
-    | "official_contact"
-    | "trackdechets_postal_mail"
-    | "verified";
+  verification_type: EmailDomainVerificationType;
   // Unused.
   can_be_suggested: boolean;
   // Can be updated when verification_type changes.
