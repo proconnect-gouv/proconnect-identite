4945246ff6b903a2124137aa62219d758df2ceb8
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-05-28 19:48:21 +0200

    chore(controller): create 2fa controller

--- src/controllers/user/double-authentication-choice.ts
+++ src/controllers/user/double-authentication-choice.ts
@@ -1,6 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
+import { getUserFromAuthenticatedSession } from "../../managers/session/authenticated";
+import {
+  getTemporaryTotpKey,
+  setTemporaryTotpKey,
+} from "../../managers/session/temporary-totp-key";
+import { generateAuthenticatorAppRegistrationOptions } from "../../managers/totp";
 import { csrfToken } from "../../middlewares/csrf-protection";
-
 export const getDoubleAuthenticationChoiceController = async (
   req: Request,
   res: Response,
@@ -8,9 +13,51 @@ export const getDoubleAuthenticationChoiceController = async (
 ) => {
   try {
     return res.render("user/double-authentication-choice", {
-      pageTitle: "Double authentification test",
+      pageTitle: "Choisir un mode de double authentification",
       csrfToken: csrfToken(req),
       illustration: "illu-2FA.svg",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getConfiguringSingleUseCodeController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    return res.render("user/configuring-single-use-code", {
+      pageTitle: "Installer votre outil d'authentification",
+      csrfToken: csrfToken(req),
+      illustration: "illu-2FA.svg",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAuthenticatorAppConfigurationController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { email } = getUserFromAuthenticatedSession(req);
+    const existingTemporaryTotpKey = getTemporaryTotpKey(req);
+    const { totpKey, humanReadableTotpKey, qrCodeDataUrl } =
+      await generateAuthenticatorAppRegistrationOptions(
+        email,
+        existingTemporaryTotpKey,
+      );
+    setTemporaryTotpKey(req, totpKey);
+    return res.render("user/authenticator-app-configuration", {
+      pageTitle: "Configurer un code à usage unique",
+      csrfToken: csrfToken(req),
+      illustration: "illu-2FA.svg",
+      humanReadableTotpKey,
+      qrCodeDataUrl,
     });
   } catch (error) {
     next(error);
