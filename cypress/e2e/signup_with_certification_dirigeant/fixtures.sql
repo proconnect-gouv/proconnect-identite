14f9c43289a14b670459fbc82803be43e504aff5
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-07-17 20:05:00 +0200

    ✅ split certification dirigeant tests into two files between sign-in and sign-up

---
+++ cypress/e2e/signup_with_certification_dirigeant/fixtures.sql
@@ -0,0 +1,19 @@
+INSERT INTO oidc_clients
+(client_name, client_id, client_secret, redirect_uris,
+ post_logout_redirect_uris, scope, client_uri, client_description,
+ userinfo_signed_response_alg, id_token_signed_response_alg,
+ authorization_signed_response_alg, introspection_signed_response_alg)
+VALUES
+  ('Oidc Test Client',
+   'standard_client_id',
+   'standard_client_secret',
+   ARRAY [
+     'http://localhost:4000/login-callback'
+     ],
+   ARRAY [
+     'http://localhost:4000/'
+    ],
+   'openid email profile organization',
+   'http://localhost:4000/',
+   'ProConnect test client. More info: https://github.com/numerique-gouv/proconnect-test-client.',
+   null, null, null, null);
