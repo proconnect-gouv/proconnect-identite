ee9e5a774a441384f70b6ade9b4389e77cca9ac0
Douglas DUTEIL <douglasduteil@gmail.com>
2025-09-19 15:18:01 +0200

    feat(database): create database schema package

--- packages/database/scripts/build.ts
+++ packages/database/scripts/build.ts
@@ -12,6 +12,7 @@ $.verbose = true;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const root = relative(__dirname, join(__dirname, "..", ".."));
+
 await $`docker compose down --volumes`;
 await $`docker compose up db --wait`;
 await $({ cwd: root })`npm run build:db:migrate`;
@@ -19,6 +20,7 @@ await $`docker compose exec -T db psql ${[
   "--username=proconnect-identite",
   '--command=DROP TABLE IF EXISTS "pgmigrations";',
 ]}`;
+
 await $`docker compose exec -T db pg_dump ${[
   "--clean",
   "--if-exists",
@@ -28,3 +30,14 @@ await $`docker compose exec -T db pg_dump ${[
   "--schema-only",
   "--username=proconnect-identite",
 ]} > schema.sql`;
+
+await $`drizzle-kit pull`;
+
+await $`sed -i 's/unknown("credential_public_key")/bytea("credential_public_key")/g' src/drizzle/schema.ts`;
+await $`echo ${'import { bytea } from "./orm/columes/bytea.js";'} >> src/drizzle/schema.ts`;
+await $`sed -i "s/${"schema"}/${"schema.js"}/g" src/drizzle/relations.ts`;
+
+await $`prettier --write src/drizzle/*.ts`;
+await $`rm -rf src/drizzle/meta src/drizzle/*.sql`;
+
+await $`docker compose down --volumes`;
