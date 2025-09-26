ea299182b28ec0066f13f2774f192fd9ea69566d
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2025-09-26 11:49:32 +0200

    💅 run 'npm run lint'

--- cypress/e2e/signin_from_standard_client/fixtures.sql
+++ cypress/e2e/signin_from_standard_client/fixtures.sql
@@ -1,38 +1,101 @@
-INSERT INTO users
-  (id, email, email_verified, email_verified_at, encrypted_password, created_at, updated_at, given_name, family_name,
-   phone_number, job)
+INSERT INTO
+  users (
+    id,
+    email,
+    email_verified,
+    email_verified_at,
+    encrypted_password,
+    created_at,
+    updated_at,
+    given_name,
+    family_name,
+    phone_number,
+    job
+  )
 VALUES
-  (1, 'unused1@yopmail.com', true, CURRENT_TIMESTAMP, '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Jean', 'Jean Un', '0123456789', 'Sbire'),
-  (2, 'unused2@yopmail.com', true, CURRENT_TIMESTAMP, '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Jean', 'Jean Deux', '0123456789', 'Sbire');
+  (
+    1,
+    'unused1@yopmail.com',
+    true,
+    CURRENT_TIMESTAMP,
+    '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    'Jean',
+    'Jean Un',
+    '0123456789',
+    'Sbire'
+  ),
+  (
+    2,
+    'unused2@yopmail.com',
+    true,
+    CURRENT_TIMESTAMP,
+    '$2a$10$kzY3LINL6..50Fy9shWCcuNlRfYq0ft5lS.KCcJ5PzrhlWfKK4NIO',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    'Jean',
+    'Jean Deux',
+    '0123456789',
+    'Sbire'
+  );
 
-
-INSERT INTO organizations
-  (id, siret, created_at, updated_at)
+INSERT INTO
+  organizations (id, siret, created_at, updated_at)
 VALUES
-  (1, '21340126800130', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-  (2, '21920023500014', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+  (
+    1,
+    '21340126800130',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+  ),
+  (
+    2,
+    '21920023500014',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+  );
 
-INSERT INTO users_organizations
-  (user_id, organization_id, is_external, verification_type, has_been_greeted)
+INSERT INTO
+  users_organizations (
+    user_id,
+    organization_id,
+    is_external,
+    verification_type,
+    has_been_greeted
+  )
 VALUES
   (1, 1, false, 'domain', true),
   (2, 1, false, 'domain', true),
   (2, 2, false, 'domain', true);
 
-INSERT INTO oidc_clients
-  (client_name, client_id, client_secret, redirect_uris,
-   post_logout_redirect_uris, scope, client_uri, client_description,
-   userinfo_signed_response_alg, id_token_signed_response_alg,
-   authorization_signed_response_alg, introspection_signed_response_alg)
+INSERT INTO
+  oidc_clients (
+    client_name,
+    client_id,
+    client_secret,
+    redirect_uris,
+    post_logout_redirect_uris,
+    scope,
+    client_uri,
+    client_description,
+    userinfo_signed_response_alg,
+    id_token_signed_response_alg,
+    authorization_signed_response_alg,
+    introspection_signed_response_alg
+  )
 VALUES
-  ('Oidc Test Client',
-   'standard_client_id',
-   'standard_client_secret',
-   ARRAY [
-     'http://localhost:4000/login-callback'
-     ],
-   ARRAY []::varchar[],
-   'openid email profile organization',
-   'http://localhost:4000/',
-   'ProConnect test client. More info: https://github.com/proconnect-gouv/proconnect-test-client.',
-   null, null, null, null);
+  (
+    'Oidc Test Client',
+    'standard_client_id',
+    'standard_client_secret',
+    ARRAY['http://localhost:4000/login-callback'],
+    ARRAY[]::varchar[],
+    'openid email profile organization',
+    'http://localhost:4000/',
+    'ProConnect test client. More info: https://github.com/proconnect-gouv/proconnect-test-client.',
+    null,
+    null,
+    null,
+    null
+  );
