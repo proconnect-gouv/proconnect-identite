631e7542f9c18739b9a7ff471c3416903831373a
BenoitSerrano <benoit.serrano10@gmail.com>
2025-11-21 18:19:21 +0100

    ✨ handle siret_hint on sign up

--- cypress/e2e/signup_with_suggestion/fixtures.sql
+++ cypress/e2e/signup_with_suggestion/fixtures.sql
@@ -6,4 +6,43 @@ VALUES
     '11009001600053',
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP
+  ),
+  (
+    2,
+    '21340126800130',
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+  );
+
+INSERT INTO
+  oidc_clients (
+    client_name,
+    client_id,
+    client_secret,
+    redirect_uris,
+    post_logout_redirect_uris,
+    scope,
+    client_uri,
+    client_description,
+    userinfo_signed_response_alg,
+    id_token_signed_response_alg,
+    authorization_signed_response_alg,
+    introspection_signed_response_alg,
+    is_proconnect_federation
+  )
+VALUES
+  (
+    'ProConnect Federation',
+    'proconnect_federation_client_id',
+    'proconnect_federation_client_secret',
+    ARRAY['http://localhost:4001/login-callback'],
+    ARRAY[]::varchar[],
+    'openid uid given_name usual_name email phone siret is_service_public is_public_service',
+    'http://localhost:4001/',
+    'Dispositif d’identification des agents de la fonction publique.',
+    'ES256',
+    'ES256',
+    'ES256',
+    'ES256',
+    true
   );
