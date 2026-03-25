df5052b39b2147a27138ac215b05470c0e48a5d5
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2026-03-25 16:21:58 +0100

    Remove residual cypress only instruction

--- cypress/e2e/signin_with_totp_enrollment/index.cy.ts
+++ cypress/e2e/signin_with_totp_enrollment/index.cy.ts
@@ -1,7 +1,7 @@
 describe("sign-in with totp enrollment", () => {
   before(cy.seed);
 
-  it.only("should follow first authentication when mfa asked", function () {
+  it("should follow first authentication when mfa asked", function () {
     cy.visit("http://localhost:4000");
 
     cy.contains("Forcer une connexion a deux facteurs").click();
