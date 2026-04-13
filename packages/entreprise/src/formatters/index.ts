bd9ba7a9e18c40ec0166a2fef050f16da8f9133d
rebeccadumazert <rebeccadumazert@hotmail.fr>
2026-04-13 17:15:26 +0200

    ✨ Added “tranche-effectifs-unite-legale” to index.ts following an omission in the last PR

--- packages/entreprise/src/formatters/index.ts
+++ packages/entreprise/src/formatters/index.ts
@@ -4,3 +4,4 @@ export * from "./adresse-establishment.js";
 export * from "./libelle-from-code-effectif.js";
 export * from "./main-activity.js";
 export * from "./nom-complet.js";
+export * from "./tranche-effectifs-unite-legale.js";
