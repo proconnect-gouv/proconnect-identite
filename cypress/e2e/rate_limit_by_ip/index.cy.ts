c6982b12d5c9fad8668f1f667ddf12e5c973bdba
Raphaël Dubigny <rdubigny@users.noreply.github.com>
2026-09-23 10:10:44 +0200

    🚦 Fix rate limiter tuning (#2166)
    * 🚦 Update rate limiter point limits
    
    **Problem**
    
    We set an arbitrary and non-customizable value for the IP-based API rate
    limiter.
    
    **Proposal**
    
    We measured 43,745 calls between 9:00 and 11:00 on September 9, 2026,
    which amounts to about 365 calls per minute.
    
    Set the number of points to roughly three times this value: 1,000.
    
    Also arbitrarily increase the number of points for the app rate limiter
    to 100 to be more resilient for users sharing the same IP address.
    
    Eventually, the number of points can be set with env variables.
    
    * 🔃 Sort alphabetically

--- cypress/e2e/rate_limit_by_ip/index.cy.ts
+++ cypress/e2e/rate_limit_by_ip/index.cy.ts
@@ -5,11 +5,11 @@ describe("trigger rate limiting by ip", () => {
   beforeEach(() => {
     // Clear rate-limiter credits
     cy.exec(
-      "docker compose exec redis redis-cli --scan --pattern 'rate-limiter:*' | xargs -r docker compose exec redis redis-cli DEL",
+      "docker compose exec redis redis-cli --scan --pattern 'rate-limiter-*' | xargs -r docker compose exec redis redis-cli DEL",
     );
   });
 
-  it("should trigger ip rate limiting when hitting 51 pages after a successful login", function () {
+  it("should trigger ip rate limiting when hitting 1 pages after a successful login", function () {
     cy.visit("http://localhost:4000");
 
     cy.get("button.proconnect-button").click();
@@ -20,8 +20,8 @@ describe("trigger rate limiting by ip", () => {
     cy.contains("standard-client");
 
     // we already consumed 9 rate limiter credits during the login process
-    for (let i = 1; i <= 60 - 9; i++) {
-      cy.visit("/", { failOnStatusCode: false });
+    for (let i = 1; i <= 10 - 9; i++) {
+      cy.visit("/");
       cy.contains("Votre compte ProConnect");
     }
 
@@ -42,7 +42,7 @@ describe("trigger rate limiting by ip", () => {
     cy.login("rate-limit+user2@yopmail.com");
 
     // we already consumed 8 rate limiter credits during the login process
-    for (let i = 1; i <= 60 - 8; i++) {
+    for (let i = 1; i <= 10 - 8; i++) {
       cy.visit("/");
       cy.contains("Votre compte ProConnect");
     }
@@ -60,7 +60,7 @@ describe("trigger rate limiting by ip", () => {
   });
 
   it("should trigger IP rate limiting by hitting 404 errors", function () {
-    for (let i = 1; i <= 60; i++) {
+    for (let i = 1; i <= 10; i++) {
       cy.visit("http://localhost:3000/random", { failOnStatusCode: false });
       cy.contains("Page non trouvée");
     }
@@ -75,7 +75,7 @@ describe("trigger rate limiting by ip", () => {
   });
 
   it("should trigger IP rate limiting by hitting 404 errors under /oauth", function () {
-    for (let i = 1; i <= 60; i++) {
+    for (let i = 1; i <= 10; i++) {
       cy.request(
         "http://localhost:3000/oauth/.well-known/openid-configuration",
       );
@@ -90,7 +90,7 @@ describe("trigger rate limiting by ip", () => {
   });
 
   it("should not trigger IP rate limiting by hitting 404 errors under /api", function () {
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 12; i++) {
       cy.visit("http://localhost:3000/api/random", { failOnStatusCode: false });
       cy.contains("Page non trouvée");
     }
